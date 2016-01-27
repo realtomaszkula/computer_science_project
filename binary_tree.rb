513e3892bbf8c517577e17b6b845126b0bceadf0
Tomasz Kula <tomaszkula07@gmail.com>
2016-01-27 17:26:51 +0100

    builds a tree out of sorted

--- binary_tree.rb
+++ binary_tree.rb
@@ -10,7 +10,7 @@ class Node
     @parent_node  = parent_node
     @child_node = child_node
 
-    @@instance_collector << self if @value != nil
+    @@instance_collector << self
   end
 
   def self.offspring
@@ -22,25 +22,35 @@ end
 def build_tree(arr, parent = nil)
 
 mid = arr.length / 2
-left = arr[0..mid]
+left = arr[0...mid]
 right = arr[mid..-1]
 value = right.shift
-
-
+value = left.shift if value == nil
 return if value == nil
-return nil if left.size ==  0 || right.size == 0
+# return nil if left.size ==  0 || right.size == 0
 
 node = Node.new(value, parent)
-node.child_node = [ Node.new(build_tree(left), node), Node.new(build_tree(right), node) ]
+child1 =  build_tree(left, node.value)
+child2 =  build_tree(right, node.value)
 
+
+ node.child_node =  if child1 == nil && child2 == nil
+                      [nil, nil]
+                    elsif child1 == nil
+                      [nil, child2.value]
+                    elsif child2 == nil
+                      [child1.value, nil]
+                    else
+                       [child1.value, child2.value]
+                    end
 node
 end
 
 
 arr = [1,2,4,5,6,7,8]
-build_tree(arr)
 
-puts arr = Node.offspring.size
+build_tree(arr)
+p arr = Node.offspring
 # arr.each do |node|
 #   puts "value: #{node.value}"
 #   puts "parent: #{node.parent_node}"
